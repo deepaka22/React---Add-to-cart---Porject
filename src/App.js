13ff41f1d79d5776535fe0dcd37b2b4d4664221a
Deepak Venkatesan <Deepakbakyalakshmi1997@gmail.com>
2023-04-18 13:23:20 +0530

    starting new project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Navbar from './bootsrap-pricing/Navbar';
-import Header from './bootsrap-pricing/Header';
-import Boot from './bootsrap-pricing/Boot';
-import PricingChart from './bootsrap-pricing/Pricingchart';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Header/>
-      <Boot/>
-      <PricingChart/> 
+       
   
     </div>
   );
